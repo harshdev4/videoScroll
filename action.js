52b8c8edb572af8b62cc0a59d6876bbf79965661
HarshWebd <upsign443@gmail.com>
2025-04-13 18:09:56 +0530

    uploading first version

---
+++ action.js
@@ -0,0 +1,25 @@
+const sections = document.querySelectorAll('.videoWrapper > video');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.currentTime = 0;
+            entry.target.play();
+        }
+        else entry.target.pause();
+    })
+}, {
+    threshold: 0.8
+});
+
+
+const sound = (e) => {
+    sections.forEach(video => video.muted = false );
+    e.target.setAttribute('class', "fa-solid fa-volume-high")
+}
+
+
+
+sections.forEach(video => {
+    observer.observe(video);
+});
