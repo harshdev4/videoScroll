6b16246ffc6687bcded18712a5fbe66a65c04f97
HarshWebd <upsign443@gmail.com>
2025-04-13 18:54:51 +0530

    toggle between audio and mute

--- action.js
+++ action.js
@@ -13,12 +13,12 @@ const observer = new IntersectionObserver(entries => {
 });
 
 
-const sound = (e) => {
-    sections.forEach(video => video.muted = false );
-    e.target.setAttribute('class', "fa-solid fa-volume-high")
+const sound = () => {
+    sections.forEach(video => {
+        video.muted = !(video.muted);
+        video.nextElementSibling.children[0].setAttribute('class', `fa-solid ${video.muted ? 'fa-volume-xmark': 'fa-volume-high'}`);
+    } );
 }
-
-
 
 sections.forEach(video => {
     observer.observe(video);
