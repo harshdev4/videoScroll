679a2d2929ec9467697068f0c54dfb581290f763
HarshWebd <upsign443@gmail.com>
2025-04-13 19:30:42 +0530

    reduce redundancy

--- action.js
+++ action.js
@@ -1,5 +1,3 @@
-const sections = document.querySelectorAll('.videoWrapper > video');
-
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -16,18 +14,48 @@ const observer = new IntersectionObserver(entries => {
 const sound = () => {
     sections.forEach(video => {
         video.muted = !(video.muted);
-        video.nextElementSibling.children[0].setAttribute('class', `fa-solid ${video.muted ? 'fa-volume-xmark': 'fa-volume-high'}`);
-    } );
+        video.nextElementSibling.children[0].setAttribute('class', `fa-solid ${video.muted ? 'fa-volume-xmark' : 'fa-volume-high'}`);
+    });
 }
+
+const container = document.querySelector('.videoContainer');
+
+for (let index = 0; index < 4; index++) {
+    const videoWrapper = document.createElement('div');
+    videoWrapper.classList.add('videoWrapper');
+
+    const video = document.createElement('video');
+    video.src = `./assets/video${index+1}.mp4`;
+    video.autoplay = true;
+    video.muted = true;
+    video.loop = true;
+
+    const playDiv = document.createElement('div');
+    playDiv.classList.add('play');
+
+    const icon = document.createElement('i');
+    icon.classList.add('fa-solid', 'fa-volume-xmark');
+    icon.onclick = sound;
+
+    container.appendChild(videoWrapper);
+
+    videoWrapper.appendChild(video);
+    videoWrapper.appendChild(playDiv);
+
+    playDiv.appendChild(icon);
+
+}
+
+const sections = document.querySelectorAll('.videoWrapper > video');
 
 sections.forEach(video => {
     observer.observe(video);
-    video.addEventListener('click', ()=> {
-       if (video.paused) {
-        video.play();
-       }
-       else{
-        video.pause();
-       }
+    video.addEventListener('click', () => {
+        if (video.paused) {
+            video.play();
+        }
+        else {
+            video.pause();
+        }
     })
 });
